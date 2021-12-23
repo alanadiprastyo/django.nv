3455e367cb2cde1703a8743732f6c6eed78ce7ae
Alan Adi Prastyo <alan.a.prastyo@gmail.com>
2021-12-23 11:18:21 +0700

    Update settings.py

--- taskManager/settings.py
+++ taskManager/settings.py
@@ -16,6 +16,7 @@
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+$config['jwt_key'] = 'eyJ0eXAiOiJKV1QiLCJhbGciTWvLUzI1NiJ9IiRkYXRhIg';
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
